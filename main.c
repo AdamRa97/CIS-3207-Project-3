6c0d7f124365b66a94d5c33fc8b1d311f4041474
AdamRa97 <adamra97@gmail.com>
2023-04-16 18:11:35 -0400

    Added structs for buffer and sycn struct variables to pass through later on

--- main.c
+++ main.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 #define BUFFER_SIZE_1 10
 #define BUFFER_SIZE_2 15
 
 // Buffer and synchronization structures
-typedef struct {
+typedef struct{
     int buffer[BUFFER_SIZE_1];
     int count;
     pthread_mutex_t mutex;
@@ -15,13 +18,20 @@ typedef struct {
     pthread_cond_t not_empty;
 } Buffer1;
 
-typedef struct {
+typedef struct{
     int buffer[BUFFER_SIZE_2];
     int count;
     pthread_mutex_t mutex;
     pthread_cond_t not_full;
     pthread_cond_t not_empty;
 } Buffer2;
+
+// Global Variable for pipe
+int pipe_fd[2];
+
+// Global Variables for buffers
+Buffer1 buffer1;
+Buffer2 buffer2;
 
 int main(){
     return 0;
